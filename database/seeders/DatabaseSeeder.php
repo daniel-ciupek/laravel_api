cd1be2e5d6520004d8ea4535113b671d8db7efb7
daniel-ciupek <danter005@o2.pl>
2026-03-02 20:29:40 +0100

    API: Manual test the feature with Postman

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Task;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Carbon\CarbonPeriod;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,8 +18,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+    $start = now()->startOfMonth()->subMonthsNoOverflow();
+        $end = now();
+        $period = CarbonPeriod::create($start, '1 day', $end);
+
      User::factory(5)
-        ->has(Task::factory()->count(10))
-        ->create();
+        ->create()
+            ->each(function ($user) use($period) {
+                foreach ($period as $date) {
+                    $date->hour(rand(0, 23))->minute(rand(0, 6) * 10);
+
+                    Task::factory()->create([
+                        'user_id' => $user->id,
+                        'created_at' => $date,
+                        'updated_at' => $date
+                    ]);
+                }
+            });
     }
 }
